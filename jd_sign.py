e168e77569ad1d8442588690f5f356820573e199
1774293824 <1774293824@qq.com>
2024-01-26 19:00:07 +0800

    优化调用接口为函数

--- jd_sign.py
+++ jd_sign.py
@@ -5,30 +5,26 @@ import http.client
 cookie_001 = os.environ.get("JD_COOKIE_001")
 cookie_002 = os.environ.get("JD_COOKIE_002")
 
-conn = http.client.HTTPSConnection("api.m.jd.com")
-url = (
-    "/client.action?functionId=signBeanAct&body=%257B%2522fp%2522%253A%2522-1%2522%252C%2522shshshfp%2522%253A%2522"
-    "-1%2522%252C%2522shshshfpa%2522%253A%2522-1%2522%252C%2522referUrl%2522%253A%2522-1%2522%252C%2522userAgent"
-    "%2522%253A%2522-1%2522%252C%2522jda%2522%253A%2522-1%2522%252C%2522rnVersion%2522%253A%25223.9%2522%257D"
-    "&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVersion=14.8.1&uuid"
-    "=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp"
-    "=jsonp_1645885800574_58482")
-headers_001 = {
-    'Cookie': cookie_001
-}
-conn.request("POST", url=url, headers=headers_001)
-res = conn.getresponse()
-response = res.read().decode("utf-8")
-json_str = response[response.index('(') + 1:response.rindex(')')]
-response_json = json.loads(json_str)
-print(response_json.get('data').get('dailyAward'))
 
-headers_002 = {
-    'Cookie': cookie_001
-}
-conn.request("POST", url=url, headers=headers_002)
-res = conn.getresponse()
-response = res.read().decode("utf-8")
-json_str = response[response.index('(') + 1:response.rindex(')')]
-response_json = json.loads(json_str)
-print(response_json.get('data').get('dailyAward'))
+def jd_sign(cookie):
+    conn = http.client.HTTPSConnection("api.m.jd.com")
+    url = (
+        "/client.action?functionId=signBeanAct&body=%257B%2522fp%2522%253A%2522-1%2522%252C%2522shshshfp%2522%253A%2522"
+        "-1%2522%252C%2522shshshfpa%2522%253A%2522-1%2522%252C%2522referUrl%2522%253A%2522-1%2522%252C%2522userAgent"
+        "%2522%253A%2522-1%2522%252C%2522jda%2522%253A%2522-1%2522%252C%2522rnVersion%2522%253A%25223.9%2522%257D"
+        "&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVersion=14.8.1&uuid"
+        "=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp"
+        "=jsonp_1645885800574_58482")
+    headers_001 = {
+        'Cookie': cookie
+    }
+    conn.request("POST", url=url, headers=headers_001)
+    res = conn.getresponse()
+    response = res.read().decode("utf-8")
+    json_str = response[response.index('(') + 1:response.rindex(')')]
+    response_json = json.loads(json_str)
+    print(response_json.get('data').get('dailyAward'))
+
+
+jd_sign(cookie_001)
+jd_sign(cookie_002)
