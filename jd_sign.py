21346958ea1843fc91b375d9cb3e9eb167f7713e
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-01-30 12:09:40 +0800

    Update jd_sign.py

--- jd_sign.py
+++ jd_sign.py
@@ -1,35 +1,41 @@
 import json
 import os
-import http.client
 import datetime
 
+import requests
 
-def jd_sign(cookie):
-    conn = http.client.HTTPSConnection("api.m.jd.com")
-    url = (
-        "/client.action?functionId=signBeanAct&body=%257B%2522fp%2522%253A%2522-1%2522%252C%2522shshshfp%2522%253A%2522"
-        "-1%2522%252C%2522shshshfpa%2522%253A%2522-1%2522%252C%2522referUrl%2522%253A%2522-1%2522%252C%2522userAgent"
-        "%2522%253A%2522-1%2522%252C%2522jda%2522%253A%2522-1%2522%252C%2522rnVersion%2522%253A%25223.9%2522%257D"
-        "&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVersion=14.8.1&uuid"
-        "=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp"
-        "=jsonp_1645885800574_58482")
-    headers_001 = {
-        'Cookie': cookie
-    }
-    conn.request("POST", url=url, headers=headers_001)
-    res = conn.getresponse()
-    response = res.read().decode("utf-8")
-    json_str = response[response.index('(') + 1:response.rindex(')')]
-    response_json = json.loads(json_str)
-    print(response_json.get('data').get('dailyAward'))
+
+def jd_signin(cookie):
+    url = ("https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A"
+           "%22-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda"
+           "%22%3A%22-1%22%2C%22rnVersion%22%3A%223.9%22%7D&appid=ld&client=apple&clientVersion=10.0.4&networkType"
+           "=wifi&osVersion=14.8.1&uuid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid"
+           "=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp=jsonp_1645885800574_58482")
+    headers = {'Cookie': cookie}
+    r = requests.post(url=url, headers=headers)
+    print(r.text)
+
+
+def iku_signin(username, password):
+    login_url = "https://ikuuu.me/auth/login"
+    data = {"email": f"{username}",
+            "passwd": f"{password}",
+            "code": ""}
+    login_res = requests.post(login_url, data)
+    print(login_res.json())
+    cookie = login_res.cookies
+    sing_headers = {"cookie": "; ".join([f"{name}={value}" for name, value in cookie.items()])}
+    sign_url = "https://ikuuu.me/user/checkin"
+    sign_res = requests.post(sign_url, headers=sing_headers, data={})
+    print(sign_res.json())
 
 
 cookie_001 = os.environ.get("JD_COOKIE_001")
 cookie_002 = os.environ.get("JD_COOKIE_002")
-jd_sign(cookie_001)
-jd_sign(cookie_002)
+jd_signin(cookie_001)
+jd_signin(cookie_002)
+
+iku_signin('a2401193521@qq.com', 'qwer1122')
 
 time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-with open('test.text', 'a+', encoding='utf8') as f:
-    f.write(f"执行完成,执行时间：{time}" + '\n')
-print(f"执行完成,执行时间：{time}")
+print(f"--=执行完成=--，执行时间：{time}")
