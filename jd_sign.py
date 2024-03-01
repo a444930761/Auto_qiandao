83eddf25d393673a83c6459696f7034aabe397cb
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-03-01 14:25:05 +0800

    Update jd_sign.py

--- jd_sign.py
+++ jd_sign.py
@@ -3,8 +3,34 @@ import os
 import datetime
 
 import requests
+import smtplib
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+def send_email():
+    sender_email = "3521850769@qq.com"
+    sender_password = "kbmhzyfrifhrdbbc"
+    receiver_email = "3521850769@qq.com"
+    subject = "GitHub Actions 发生错误"
+    body = f"您的 GitHub Actions 发生了错误，请尽快处理。{time.strftime("%Y-%m-%d")}"
 
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+    try:
+        server = smtplib.SMTP("smtp.qq.com", 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        print("邮件发送成功")
+    except Exception as e:
+        print("邮件发送失败")
+        print(e)
+    finally:
+        server.quit()
 def jd_signin(cookie):
     url = ("https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A"
            "%22-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda"
@@ -30,13 +56,17 @@ def iku_signin(username, password):
     sign_url = "https://ikuuu.pw/user/checkin"
     sign_res = requests.post(sign_url, headers=sing_headers, data={})
     print(sign_res.json())
+    return login_res.json()
 
 
 cookie_001 = os.environ.get("JD_COOKIE_001")
 iku_psd = os.environ.get("IKU_PSD")
 jd_signin(cookie_001)
 
-iku_signin('a2401193521@qq.com', f'{iku_psd}')
-
+assert_data = iku_signin('a2401193521@qq.com', f'{iku_psd}')
+if str(assert_data) == '{'ret': 1, 'msg': '登录成功'}':
+    send_email()
+else:
+    pass
 time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"--=执行完成=--，执行时间：{time}")
