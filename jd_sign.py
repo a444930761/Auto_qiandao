12edb3ce29e8837937305a4e5a894dec8d6f5faf
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-03-13 10:24:29 +0800

    Update jd_sign.py

--- jd_sign.py
+++ jd_sign.py
@@ -1,6 +1,7 @@
 import json
 import os
-import datetime
+from datetime import datetime
+from re import T
 import requests
 import smtplib
 import time
@@ -10,8 +11,6 @@ from email.mime.text import MIMEText
 JD_cookie = os.environ.get("JD_COOKIE_001")
 iku_psd = os.environ.get("IKU_PSD")
 email_pwd = os.environ.get("EMAIL_PASSWORD")
-
-
 
 def send_email(subject, body):
     sender_email = "3521850769@qq.com"
@@ -31,44 +30,57 @@ def send_email(subject, body):
 
 
 def jd_signin(cookie):
-    url = ("https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A%2"
-           "2-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda%22%3A"
-           "%22-1%22%2C%22rnVersion%22%3A%223.9%22%7D&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVer"
-           "sion=14.8.1&uuid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6"
-           "&jsonp=jsonp_1645885800574_58482")
-    headers = {"Cookie": cookie,
-               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHT"
-                             "ML, like Gecko)Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
+    url = (
+        "https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A%2"
+        "2-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda%22%3A"
+        "%22-1%22%2C%22rnVersion%22%3A%223.9%22%7D&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVer"
+        "sion=14.8.1&uuid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6"
+        "&jsonp=jsonp_1645885800574_58482"
+    )
+    headers = {
+        "Cookie": cookie,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHT"
+                      "ML, like Gecko)Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
+    }
     r = requests.post(url=url, headers=headers)
-    json_data = r.text.replace('jsonp_1645885800574_58482(', '').rstrip(');')
-    data = json.loads(json_data)
-    print(data)
+    json_data = r.text.replace("jsonp_1645885800574_58482(", "").rstrip(");")
+    res_title = json.loads(json_data)
+    res_title = res_title.get('data').get('dailyAward').get('title')
+    return res_title
 
 
 def iku_signin(username, password):
     login_url = "https://ikuuu.pw/auth/login"
-    data = {"email": f"{username}",
-            "passwd": f"{password}",
-            "code": ""}
+    data = {"email": f"{username}", "passwd": f"{password}", "code": ""}
     login_res = requests.post(login_url, data)
-    print(str(login_res.json()))
+    print(login_res.json())
     cookie = login_res.cookies
-    sing_headers = {"cookie": "; ".join([f"{name}={value}" for name, value in cookie.items()])}
+    sing_headers = {
+        "cookie": "; ".join([f"{name}={value}" for name, value in cookie.items()])
+    }
     sign_url = "https://ikuuu.pw/user/checkin"
     sign_res = requests.post(sign_url, headers=sing_headers, data={})
-    print(str(sign_res.json()))
+    print(sign_res.json())
     return login_res.json()
 
 
-jd_signin(JD_cookie)
+title = jd_signin(JD_cookie)
+if str(title) not in ("签到成功，", "今天已签到，"):
+    subject = "GitHub Actions 运行异常"
+    body = f"您的 GitHub Actions 发生了错误，请尽快处理。错误时间：{time.strftime("%Y-%m-%d")}"
+    send_email(subject, body)
+    print("发送告警邮件")
+else:
+    print(f"京东签到成功，签到时间:{time_now}")
 
 assert_data = iku_signin('a2401193521@qq.com', f'{iku_psd}')
+
 if str(assert_data) != "{'ret': 1, 'msg': '登录成功'}":
-    subject = "GitHub Actions 发生错误"
-    body = f"您的 GitHub Actions 发生了错误，请尽快处理。{time.strftime("%Y-%m-%d")}"
+    subject = "GitHub Actions 运行异常"
+    body = f"您的 GitHub Actions 发生了错误，请尽快处理。错误时间：{time.strftime("%Y-%m-%d")}"
     send_email(subject, body)
     print("发送告警邮件")
 else:
     pass
-time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 print(f"--=执行完成=--，执行时间：{time_now}")
